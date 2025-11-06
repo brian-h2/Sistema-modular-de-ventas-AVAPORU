4c8c37ca98b6a2f47e0a987357fea5b61251f518
brian-h2 <brianheredia200309@gmail.com>
2025-11-06 15:54:02 -0300

    Asignamos metodo para cambiar el estado de una venta

--- server/routes/sales.routes.js
+++ server/routes/sales.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createSale, getSale } from "../controllers/sales.controller.js";
+import { createSale, getSale, updateSaleStatus } from "../controllers/sales.controller.js";
 const r = Router();
 
 r.post("/", createSale);
 r.get("/", getSale);   
 r.get("/:id", getSale);
+r.put("/:id/estado", updateSaleStatus);
 
 export default r;
